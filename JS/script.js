aff7d76a6e0e441177a8ffa2280a68e5faa5b45b
GiacWebDev <giacobechini@gmail.com>
2023-09-14 16:40:28 +0200

    [edit] js structure

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+// 1 scrivo l'array con la lista della spesa
+// 2 creo il contatore con la sua variabile fuori da tutto
+// 3 creo il ciclo while
+// 4 esco dal ciclo
+
+const outputEl = document.querySelector('.output')
+const listaSpesa = ['banane', 'lamponi', 'ciliegie', 'mandarini', 'carote', 'dentifricio'];
+
+let counter = 0;
+let fineLista = false;
+
+while ( counter < listaSpesa.length) {
+  const item = listaSpesa[counter];
+  counter++
+  console.log(item);
+
+  if (counter === listaSpesa.length) 
+  break;
+    
+
+  outputEl.innerHTML = item
+}
+
+
+
+
